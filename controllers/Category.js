58b8d4e38796a793c25e4b84fc0984530121a669
1sisodiyaji <637golusingh@gmail.com>
2024-05-21 13:48:46 +0530

    changes done

--- controllers/Category.js
+++ controllers/Category.js
@@ -30,7 +30,8 @@ exports.createCategory = async (req, res) => {
 
 exports.showAllCategories = async (req, res) => {
   try {
-    const allCategorys = await Category.find()
+    const allCategorys = await Category.findAll();
+    console.log(allCategorys);
     res.status(200).json({
       success: true,
       data: allCategorys,
