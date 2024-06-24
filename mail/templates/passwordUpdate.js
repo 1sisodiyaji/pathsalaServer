a1e45011c4f455c35bf8440157e1a0bcc9b1aa93
1sisodiyaji <637golusingh@gmail.com>
2024-06-24 17:00:00 +0530

    update

--- mail/templates/passwordUpdate.js
+++ mail/templates/passwordUpdate.js
@@ -55,8 +55,8 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+        <a href="https://pathshala-ruby.vercel.app"><img class="logo"
+        src="https://pathshala-ruby.vercel.app/logo.png" alt="Pathsala Logo" title = "Pathsala" ></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
@@ -64,10 +64,8 @@ exports.passwordUpdated = (email, name) => {
                 </p>
                 <p>If you did not request this password change, please contact us immediately to secure your account.</p>
             </div>
-            <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
-                at
-                <a href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!
-            </div>
+            <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
+			href="mailto:Connect.pathsala@gmail.com">Connect.pathsala@gmail.com</a>. We are here to help!</div>
         </div>
     </body>
     
